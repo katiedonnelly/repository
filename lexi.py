70901f862a63c4e07401a3180f0b70a3964bde3b
Alexa Wildy <alexawildy@gmail.com>
2021-01-23 12:03:08 -0600

    in out

--- lexi.py
+++ lexi.py
@@ -1,3 +1,25 @@
-#lexi is cute
+# lexi is cute
 
-print(helllo,world)
+print('helllo,world')
+# import pippip.main (['install','pyfirmata'])
+# from pyfirmata import Arduino,util
+
+ # try: from pyfirmata import Ardunio, util
+ # except:
+# import pippip.main('install','pyfirmata']) from pyfirmata import Arduino,util
+# board = arduino('COM3')
+# iterator = util.Iterator(board)
+# interator.start()
+# something = board.get_pin('a:0:1')
+nuber =12+12+6
+
+print(nuber)
+countin=0
+countout=0
+total =0
+if (something>15 and something<30):
+    countin = countin+1
+if (something<15 and something>0):
+    countout = countout+1
+total = countin-countout
+print(total)

f15bf5ccded491890ee8ff88896badd821ab74a6
Alexa Wildy <alexawildy@gmail.com>
2021-01-23 15:39:37 -0600

    while wohooo

--- lexi.py
+++ lexi.py
@@ -1,43 +1,38 @@
 # lexi is cute
 
-print('helllo,world')
+print('hello,world')
 # import pippip.main (['install','pyfirmata'])
 # from pyfirmata import Arduino,util
 
- # try: from pyfirmata import Ardunio, util
- # except:
-# import pippip.main('install','pyfirmata']) from pyfirmata import Arduino,util
-# board = arduino('COM3')
-# iterator = util.Iterator(board)
-# interator.start()
-# something = board.get_pin('a:0:1')
+try:
+    from pyfirmata import Ardunio, util
+except:
+    import pip
+    pip.main (['install','pyfirmata'])
+    from pyfirmata import Arduino,util
+board = Arduino('COM6')
+iterator = util.Iterator(board)
+iterator.start()
+something = board.get_pin('a:0:1')
+
+
+
 countin=0
 countout=0
 total =0
-something =16
-while(1==1):
-    if (something>15 and something<30):
-        countin = countin+1
-    if (something<15 and something>0):
-        countout = countout+1
-    total = countin-countout
-    print(total)
+# something =16
+# while(1==1):
+    # if (something>15 and something<30):
+      #  countin = countin+1
+    # if (something<15 and something>0):
+      #   countout = countout+1
+    # total = countin-countout
+    # print(total)
 
 
 
 
-cat=5
-dog=8
-hamster = 10
 
-if (cat==5):
-    print('hi cat')
-    if(dog==8):
-        print(' hi dog')
-        if(hamster==9):
-            print('hi hamster')
-        elif (hamster ==10):
-            print('sup')
 
 
 

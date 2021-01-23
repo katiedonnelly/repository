36110f75687fbcc49e49438ca3d7d9a9fe0221e8
Alexa Wildy <alexawildy@gmail.com>
2021-01-23 14:30:20 -0600

    while wohooo

--- lexi.py
+++ lexi.py
@@ -11,15 +11,44 @@ print('helllo,world')
 # iterator = util.Iterator(board)
 # interator.start()
 # something = board.get_pin('a:0:1')
-nuber =12+12+6
-
-print(nuber)
 countin=0
 countout=0
 total =0
-if (something>15 and something<30):
-    countin = countin+1
-if (something<15 and something>0):
-    countout = countout+1
-total = countin-countout
-print(total)
+something =16
+while(1==1):
+    if (something>15 and something<30):
+        countin = countin+1
+    if (something<15 and something>0):
+        countout = countout+1
+    total = countin-countout
+    print(total)
+
+
+
+
+cat=5
+dog=8
+hamster = 10
+
+if (cat==5):
+    print('hi cat')
+    if(dog==8):
+        print(' hi dog')
+        if(hamster==9):
+            print('hi hamster')
+        elif (hamster ==10):
+            print('sup')
+
+
+
+
+
+
+
+
+
+
+
+
+
+

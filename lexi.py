a5597fea3a51caa5b06ac0c603c51a1d64b8c3de
Alexa Wildy <alexawildy@gmail.com>
2021-01-24 10:40:58 -0600

    random example

--- lexi.py
+++ lexi.py
@@ -1,26 +1,31 @@
 # lexi is cute
 
-print('hello,world')
-# import pippip.main (['install','pyfirmata'])
-# from pyfirmata import Arduino,util
 
-try:
-    from pyfirmata import Ardunio, util
-except:
-    import pip
-    pip.main (['install','pyfirmata'])
-    from pyfirmata import Arduino,util
-board = Arduino('COM6')
-iterator = util.Iterator(board)
-iterator.start()
-something = board.get_pin('a:0:1')
+# connecting arduino to python
+# try:
+  #   from pyfirmata import Ardunio, util
+# except:
+    # import pip
+   #  pip.main (['install','pyfirmata'])
+    # from pyfirmata import Arduino,util
 
+import random
+import time
+
+
+# board = ArduinoMEGA('COM6')
+#  theloop = util.Iterator(board)
+# theloop.start()
+# something = board.get_pin('d:8')
+# time.sleep(2.0)
+# print(something.read())
 
 
 countin=0
 countout=0
-total =0
-# something =16
+total =random.randint(0,10)
+voterbooths=int(input('how many booths are at the polling location '))
+
 # while(1==1):
     # if (something>15 and something<30):
       #  countin = countin+1
@@ -28,6 +33,29 @@ total =0
       #   countout = countout+1
     # total = countin-countout
     # print(total)
+
+something= random.randint(0,500)
+randtime = random.randint(0,900)
+current = random.randint(0,500)
+previous = random.randint(0,500)
+while(1==1):
+    diff = current - previous
+    diff = abs(diff)
+    if (diff>30):
+        if (something>350 and something<500):
+            countin = countin+1
+        if (something<350 and something>0):
+            countout = countout+1
+    total = total +countin-countout
+
+    # print(total)
+    people_in_line = total-voterbooths
+    go_to_next_available_booth = voterbooths - total
+    if (voterbooths==20): print ("allow 20 countin ")
+    if (voterbooths<total): print ("there are "+ str(people_in_line) + " line")
+    if (voterbooths>total): print ("There are " + str(go_to_next_available_booth) +" booths available")
+    time.sleep (randtime)
+
 
 
 
